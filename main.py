c2d20a400cf4950f6d16f56b257ac56770acca9c
Chen Cheng <happylifedev001@gmail.com>
2023-03-23 01:22:17 +0900

    add workdaysOnly argument

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python
+
 import subprocess
 import shutil
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import random
+import argparse
 
-def getDateList(s:datetime, e:datetime, w_freq:tuple, d_freq:tuple) -> list:
+def getDateList(s:date, e:date, w_freq:tuple, d_freq:tuple, workday:bool) -> list:
     res_list = []
-    current_day = s    
+    current_day = s 
     
     # current_weekday =  current_day.weekday()
     # delta_days = (7 - current_weekday) % 7
@@ -16,14 +18,15 @@ def getDateList(s:datetime, e:datetime, w_freq:tuple, d_freq:tuple) -> list:
     while True:        
         # select days
         next_week_date = current_day + timedelta(days=7)
-        week_days = random.sample(range(7), random.randint(*w_freq))
-        for days in week_days:
+        week_days = random.sample(range(5 if workday else 7), random.randint(*w_freq))
+        for days in sorted(week_days):
             commit_hours = random.sample(range(9, 20), random.randint(*d_freq))
             current_day += timedelta(days)
             
             if current_day >= e:
                 break
             
+
             for hour in commit_hours:
                 minute = random.randint(0, 59)
                 second = random.randint(0, 59)
@@ -35,19 +38,20 @@ def getDateList(s:datetime, e:datetime, w_freq:tuple, d_freq:tuple) -> list:
             break
     return res_list
 
-def make_commit_history(start_date:datetime, end_date:datetime, weekly_commit=(1,5), day_commit = (0,3)):
+def make_commit_history(start_date:date, end_date:date, weekly_commit=(1,5), day_commit = (0,3), workday=False):
     
+    print('make local repository')
     dir_name = 'my-history'    
     # check directory
     if os.path.exists(dir_name):
-        shutil.rmtree(dir_name)
+        os.rmdir(dir_name)
     os.mkdir(dir_name)
     os.chdir(dir_name)
-
+    print('=== Git History Create ===')
     subprocess.run(["git", "init"])
-
+    print("Creating history...")
     # make datetime list
-    date_list = getDateList(start_date, end_date, weekly_commit, day_commit)
+    date_list = getDateList(start_date, end_date, weekly_commit, day_commit, workday)
     for date in date_list:
         date_string = date.isoformat()
         # create a file
@@ -58,14 +62,24 @@ def make_commit_history(start_date:datetime, end_date:datetime, weekly_commit=(1
         commit_message = random.choice(['add file via upload', 'modify a func', 'delete file'])
 
         subprocess.run(["git", "add", "."])
-        subprocess.run(["git", "commit", f"--date={date_string}", "-m", f"{commit_message}"])
-
+        subprocess.run(["git", "commit", f"--date={date_string}", "-m", f"{commit_message}", "--quiet"])
+    print(f"=== finished (commits:{len(date_list)}, {date_list[0]}-{date_list[-1]}) ===")
     
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--startDate", default=None, help="start date for git history. default: a year ago from now")
+    parser.add_argument("-e", "--endDate", default=None, help="start date for git history. default: a year ago from now")
+    parser.add_argument("-w","--weelyFreq", default='3..5', help="the range of number of days commiting in a week")
+    parser.add_argument("-c","--commitPerDay", default='0..3', help="the range of number of commits per day")
+    parser.add_argument("-wd", "--workdaysOnly", action="store_true", help="commit in only workdays")
+
+    args = parser.parse_args()
     
-    make_commit_history(
-        datetime(2023, 3, 10),
-        datetime(2023, 4, 1),
-        (1, 5),
-        (0, 3))
+    end_date =  date.today() if args.endDate is None else date.fromisoformat(args.endDate)
+    start_date =  date(end_date.year - 1, end_date.month, end_date.day) if args.startDate is None else date.fromisoformat(args.startDate)
+    workdays_flag = args.workdaysOnly
+    weekly_freq = tuple(map(int, args.weelyFreq.split('..')))
+    commit_day= tuple(map(int , args.commitPerDay.split('..')))
+
+    make_commit_history(start_date, end_date, weekly_freq, commit_day, workdays_flag)
